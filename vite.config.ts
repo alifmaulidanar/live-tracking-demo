f8b1d11fc75b0061ab8249066e04aa2734c9e137
alifmaulidanar <alifmaulidanr@gmail.com>
2024-12-09 23:48:12 +0700

    Added PWA assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,77 @@
-import path from "path"
+import fs from 'fs';
+import path from 'path';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',  // Service worker is updated on page reload
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Location Tracking App',
+        short_name: 'Trackify',
+        description: 'Real-time location tracking app',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            "src": "public/logo/icons/manifest-icon-192.maskable.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "public/logo/icons/manifest-icon-192.maskable.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "public/logo/icons/manifest-icon-512.maskable.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "public/logo/icons/manifest-icon-512.maskable.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ]
+      },
+      workbox: {
+        // Enable Background Sync
+        runtimeCaching: [
+          {
+            handler: 'NetworkOnly',
+            urlPattern: /\/api\/.*\/*.json/,
+            method: 'POST',
+            options: {
+              backgroundSync: {
+                name: 'location-sync-queue',  // Queue name for background sync
+                options: {
+                  maxRetentionTime: 24 * 60,  // Retry for max of 24 hours (specified in minutes)
+                },
+              },
+            },
+          },
+        ],
+      },
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, './ssl/localhost-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, './ssl/localhost.pem')),
     },
   },
 })
